8f365d0b2aeb92b60a22a1881b17a2ee7cc73aa9
kurochan <kuro@kurochan.org>
2014-04-06 21:28:50 +0900

    NGワード機能追加

--- job/pakutwi.rb
+++ job/pakutwi.rb
@@ -1,8 +1,22 @@
 class PakuTwiJob
   def call
-    status = Tweet.where("screen_name = '#{CONFIG['bot']['target']}'").
-      where("text not like '%@%'").
-      where("text not like '%http%'").sample
-    TwitterClient.instance.update status.text
+    3.times do
+      status = Tweet.where("screen_name = '#{CONFIG['bot']['target']}'").
+        where("text not like '%@%'").
+        where("text not like '%http%'").sample
+
+      next if ng? status.text
+      TwitterClient.instance.update status.text
+      break
+    end
+  end
+
+  def ng?(str)
+    ret = false
+    CONFIG['bot']['ng-word'].each do |word|
+      ret = str.include?(word)
+      break if ret
+    end
+    return ret
   end
 end
